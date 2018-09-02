d7a44445429cc2ddb6edd82f93cd0553b24a0988
Kamil Albrycht <ponciusz@gmail.com>
2018-09-02 19:29:05 +0100

    travis

--- src/components/Testable/Testable.js
+++ src/components/Testable/Testable.js
@@ -1,27 +1,37 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-class Testable extends Component{
+class Testable extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: false,
+      list: ["one", "two", "three"]
+    };
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            status: false
-        }
-    }
+  renderList() {
+    return this.state.list.map((el, index) => <li key={index}>{el}</li>);
+  }
 
-    render() {
-        return(
-            <div>
-                <h3>Hello</h3>
-                
-                <p>Currently status is {this.state.status ? 'ON' : 'OFF'}</p>
+  render() {
+    return (
+      <div>
+        <h3>Hello</h3>
+        <p>Currently status is {this.state.status ? "ON" : "OFF"}</p>
+        <button
+          className="toggleState"
+          onClick={() => {
+            this.setState({ status: !this.state.status });
+          }}
+        >
+          Toggle status
+        </button>
 
-                <button className="toggleState" onClick={() => {this.setState({status: !this.state.status})}}>Toggle status</button>
-            </div>
-        )
-    }
-
+        {this.state.list.length > 0 && <ul>{this.renderList()}</ul>}
+      </div>
+    );
+  }
 }
 
-export default Testable
+export default Testable;
